1cfb11c1e1bc9a9c501bff5bde83d5076a813a97
Gabriele <gabrieleventisei@gmail.com>
2024-11-17 17:08:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 function creaArray(A){
+    arr = []
+    for(i of A){
+        num = []
+        while(i > 1){
+            num.push(i)
+            if(i % 2){
+                i = i*3 + 1
+            }else{
+                i =i/2
+            }
+        }
+        num.push(1)
+        arr.push(num)
+    }
+    return arr
+}
+
+console.log(creaArray([24,21,40]))
+
+
+function TransAlbero(){
     
 }
